9b1573f70fc6c460729aec52e03961a3f00086c6
Ced <cedric.frenken@gmail.com>
2025-08-01 12:16:07 +0200

    adjust some css

--- views/home2.js
+++ views/home2.js
@@ -2,7 +2,23 @@ export default () => `
   <!-- ========= Hero ========= -->
   <h1>Sicherheit, durch Technik</h1>
 
-  <img src="./images/fps_logo.png" alt="fps logo" style="max-width: 100%; height: auto;">
+  <img src="./images/fps_logo.png" alt="fps logo" style="max-width: 100%; height: 75vh;">
+
+  <section class="hero">
+    <p>
+      FP Sicherheitstechnik, ein kleines familiäres Unternehmen. 
+    </p>
+    <p>
+      Seit über 15 Jahren planen, installieren und warten wir maßgeschneiderte Sicherheitskonzepte. Kompetenz, Zuverlässigkeit und Diskretion machen uns zum vertrauensvollen Partner.
+    </p>
+    <p>
+      Das Unternehmen konzentriert sich auf Sicherheitstechnik speziell der DGUV V3 Prüfung und Instandhaltungsmaßnahme in ganz Deutschland.
+    </p>
+    <p>
+      Zögern Sie nicht, kontaktieren sie uns gerne.
+    </p>
+  </section>
+
 
   <section class="hero">
     <p>
